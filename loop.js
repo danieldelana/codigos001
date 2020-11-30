be6ebaccb5cd27c52b4872d37c8bf011e7c7dd32
danieldelana <danieldelana@hotmail.com>
2020-11-30 13:16:43 -0400

    loop v0.4
    resultado foi igual a v0.3

--- loop.js
+++ loop.js
@@ -5,7 +5,10 @@ var arrayBidimensional=[
     ['azul', 'celular','golfe']
 ]
 
-for(a in arrayBidimensional)
-for(b in arrayBidimensional)
-console.log(arrayBidimensional[a][b]);
+// for(a in arrayBidimensional)
+// for(b in arrayBidimensional)
+// console.log(arrayBidimensional[a][b]);
 // resultado devolveu espalhado os valores mas com undefined no entre as chaves
+for(i=0;i<arrayBidimensional.length;i++)
+for(j=0;j<arrayBidimensional.length;j++)
+console.log(arrayBidimensional[i][j]);
